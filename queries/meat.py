e89ba0ed52b2a3f8841a3d65023d6bf1ea3e45b5
Alvise Carazzolo <alvisecarazzolo@gmail.com>
2025-06-15 11:33:35 +0200

    Remove COMMON_PARAMS usage and add params dataclasses

--- queries/meat.py
+++ queries/meat.py
@@ -1,5 +1,27 @@
+from dataclasses import dataclass, fields
+from datetime import datetime
+from typing import Optional
+
 from . import BaseQuery
-from ..enums import COMMON_PARAMS
+
+
+@dataclass
+class MeatPricesParams:
+    """Allowed parameters for the meat prices endpoint."""
+
+    memberStateCodes: Optional[str] = None
+    categories: Optional[str] = None
+    productCodes: Optional[str] = None
+    marketingYears: Optional[str] = None
+    weeks: Optional[str] = None
+    months: Optional[str] = None
+    beginDate: Optional[datetime] = None
+    endDate: Optional[datetime] = None
+
 
 class MeatPricesQuery(BaseQuery):
-    VALID_PARAMS = COMMON_PARAMS.keys()
+    VALID_PARAMS = {f.name for f in fields(MeatPricesParams)}
+
+    def __init__(self, **params):
+        super().__init__(**params)
+        self.params_obj = MeatPricesParams(**params)
