043d3f28a17b17df32478df673c7831f9ced7557
carazzolo <alvisecarazzolo@gmail.com>
2025-06-16 15:51:42 +0200

    modificata enumerazione oilseeds

---
+++ build/lib/agridata/__init__.py
@@ -0,0 +1,8 @@
+from .service import AgriDataService
+from .exceptions import AgriDataError, AgriDataHTTPError
+
+__all__ = [
+    "AgriDataService",
+    "AgriDataError",
+    "AgriDataHTTPError",
+]
