d8a01a132308eeb4c686dc4ace0ef34d4c8d20f7
carazzolo <alvisecarazzolo@gmail.com>
2025-06-16 14:52:32 +0200

    da modificare oilseed in oilseeds

--- build/lib/agridata/__init__.py
+++
@@ -1,8 +0,0 @@
-from .service import AgriDataService
-from .exceptions import AgriDataError, AgriDataHTTPError
-
-__all__ = [
-    "AgriDataService",
-    "AgriDataError",
-    "AgriDataHTTPError",
-]
--- build/lib/agridata/config.py
+++
@@ -1,2 +0,0 @@
-API_BASE_URL = "https://ec.europa.eu/agrifood/api"
-DEFAULT_TIMEOUT = 10
